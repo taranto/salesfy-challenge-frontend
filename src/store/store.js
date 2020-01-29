4a994af34480de0d7f33c8ecdac19c5f156c6cd8
Juliano H <juliano0_fsa@hotmail.com>
2020-01-29 16:58:52 -0300

    tried adjustments for deploy

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
 import rootReducer from './rootReducer';
-import { convertNumber } from '../store/converter-reducer/sagas';
 import { persistStore } from 'redux-persist';
 
-
-const sagaMiddleware = createSagaMiddleware();
-
-const middlewares = [logger, sagaMiddleware];
+const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
-
-sagaMiddleware.run(convertNumber);
 
 export default { store, persistor };
